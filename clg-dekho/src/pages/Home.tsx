cb93f9bc9b4c9024cff515da063f4c185b1bab6c
itxsoumya <itxsoumyaranjan@gmail.com>
2024-02-13 09:19:45 +0530

    frontend search

--- clg-dekho/src/pages/Home.tsx
+++ clg-dekho/src/pages/Home.tsx
@@ -8,9 +8,24 @@ const Home = ()=>{
             <div className="sm:text-6xl text-4xl font-semibold text-center pt-20 underline decoration-pink-500">
                 Not Time. Choose Your College Wisely and Bravely.
             </div>
+            {/* search section */}
             <div className="py-8">
                 <div className="flex gap-2 max-w-4xl mx-auto">
                     <Input className="text-lg py-6" type="text" placeholder="Search your college..." /> <Button className="text-lg p-6">Search</Button>
+                </div>
+            </div>
+
+            {/* list of college */}
+            <div>
+                <div className="flex gap-2 border-2 p-2 rounded-lg">
+                    <div>
+                        <img className="sm:w-60 sm:h-60 w- h-36 rounded-sm shadow-md" src="https://www.colgate.edu/sites/default/files/styles/optimized/public/2023-08/wallpaper_sunset_desktop.jpg?itok=tslDPST1" alt="" />
+                    </div>
+                    <div>
+                        {/* title */}
+                        <h2 className="text-2xl">College Of No where</h2>
+                        <h3>this is famous for its stupid syllabus</h3>
+                    </div>
                 </div>
             </div>
         </div>
