aa1d244f8a880aa470e13f819fda55db94054f70
itxsoumya <itxsoumyaranjan@gmail.com>
2024-02-14 12:02:17 +0530

    college section

--- clg-dekho/src/pages/College.tsx
+++ clg-dekho/src/pages/College.tsx
@@ -1,11 +1,158 @@
 
-const CollegePage = ()=>{
-    return(
+const CollegePage = () => {
+    return (
         <div className="bg-red-100v max-w-7xl mx-auto p-4">
-        
-            <div>
-                Ratings
+            <div className="flex max-lg:flex-col gap-4">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/berry-college-historic-campus-at-twilight-royalty-free-image-1652127954.jpg" className=" lg:w-1/2 rounded-sm shadow-md" alt="" />
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-3xl">Some Random College Of Technology</h1>
+                    <div className="flex gap-2 opacity-50">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                        </svg> some place on earth
+
+                    </div>
+                    {/* about */}
+                    <div className="italic font-serif text-2xl opacity-80">
+                        "
+                        This college is well known college in it's locality making 100% placement assurance with out any doubt. It was established in year 2000 and still up and running for what ever reason."
+                    </div>
+                    {/* rating */}
+                    <div className="">
+                        <div className="flex items-center mb-2 mt-6 hi">
+                            <svg
+                                className="w-4 h-4 text-yellow-300 me-1"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 22 20"
+                            >
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg
+                                className="w-4 h-4 text-yellow-300 me-1"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 22 20"
+                            >
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg
+                                className="w-4 h-4 text-yellow-300 me-1"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 22 20"
+                            >
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg
+                                className="w-4 h-4 text-yellow-300 me-1"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 22 20"
+                            >
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg
+                                className="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 22 20"
+                            >
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+                                4.95
+                            </p>
+                            <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+                                out of
+                            </p>
+                            <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+                                5
+                            </p>
+                        </div>
+                        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                            1,745 global ratings
+                        </p>
+                        <div className="flex items-center mt-4">
+                            <a
+                                href="#"
+                                className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                                5 star
+                            </a>
+                            <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                                <div className="h-5 bg-yellow-300 rounded" style={{ width: "70%" }} />
+                            </div>
+                            <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                                70%
+                            </span>
+                        </div>
+                        <div className="flex items-center mt-4">
+                            <a
+                                href="#"
+                                className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                                4 star
+                            </a>
+                            <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                                <div className="h-5 bg-yellow-300 rounded" style={{ width: "17%" }} />
+                            </div>
+                            <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                                17%
+                            </span>
+                        </div>
+                        <div className="flex items-center mt-4">
+                            <a
+                                href="#"
+                                className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                                3 star
+                            </a>
+                            <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                                <div className="h-5 bg-yellow-300 rounded" style={{ width: "8%" }} />
+                            </div>
+                            <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                                8%
+                            </span>
+                        </div>
+                        <div className="flex items-center mt-4">
+                            <a
+                                href="#"
+                                className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                                2 star
+                            </a>
+                            <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                                <div className="h-5 bg-yellow-300 rounded" style={{ width: "4%" }} />
+                            </div>
+                            <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                                4%
+                            </span>
+                        </div>
+                        <div className="flex items-center mt-4">
+                            <a
+                                href="#"
+                                className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                            >
+                                1 star
+                            </a>
+                            <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                                <div className="h-5 bg-yellow-300 rounded" style={{ width: "1%" }} />
+                            </div>
+                            <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                                1%
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
+            {/*  */}
         </div>
     )
 }
